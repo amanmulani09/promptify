3985ec8e6015b66e99c7873c0ce88528d3b2b3b5
amanmulani09 <mulaniaman0504@gmail.com>
2023-07-08 23:45:47 +0530

    added next auth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -14,7 +14,8 @@ const Nav = () => {
       const response = await getProviders();
       setProviders(response)
     }
-  })
+    setProviders();
+  },[])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
